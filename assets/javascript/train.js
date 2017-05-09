4ada508f0eabd9e8e5c5d9f693324bdd30880c2e
Marina James <MarinaJames@Marinas-MacBook-Air.local>
2017-05-09 18:24:48 -0400

    have blank spacesadding onto table, still debugging it

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -13,4 +13,58 @@ var config = {
   messagingSenderId: "561603915711"
 };
   firebase.initializeApp(config);
-  
+
+// Create a variable to reference the database
+    var database = firebase.database();
+    // Initial Values
+    var trainName = "";
+    var destination = "";
+    var trainTime = "";
+    var frequency = "";
+
+// Capture Button Click
+  $("#submitBtn").on("click", function() {
+    // Don't refresh the page!
+    event.preventDefault();
+
+    console.log("I was clicked!!!!");
+
+      trainName = $("#trainName").val().trim();
+      destination = $("#destination").val().trim();
+      trainTime = $("#trainTime").val().trim();
+      frequency = $("#frequency").val().trim();
+
+      database.ref().push({
+        "TrainName": trainName,
+        "Destination": destination,
+        "TrainTime": trainTime,
+        "Frequency": frequency
+      });
+    });
+        // Firebase watcher + initial loader HINT: .on("value")
+ database.ref().on("value", function(snapshot){
+
+    var sv = snapshot.val()
+    var svKeys = Object.keys(sv);
+    var objLen = svKeys.length - 1;
+
+    $("#empTable").empty();
+
+    svKeys.forEach(function(lineItem){
+      console.log(sv[lineItem].trainName, sv[lineItem].destination, sv[lineItem].trainTime, sv[lineItem].frequency);
+      addRow(sv[lineItem].trainName, sv[lineItem].destination, sv[lineItem].trainTime, sv[lineItem].frequency);
+    });
+
+  });
+
+  function addRow (){
+
+
+    $('#dataTable').append(  
+      '<tr>' + 
+        '<td>' + trainName + '</td>' +                
+        '<td>' + destination + '</td>' +                 
+        '<td>' + trainTime + '</td>' +                    
+        '<td> $' + frequency + '</td>' +               
+      '</tr>')
+    };
